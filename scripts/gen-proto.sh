5f712e102ea33c132ebc0f1015040fd90198d173
Madiyor Abdukhashimov <abdukhashimov@yandex.ru>
2022-01-19 12:38:38 +0000

    Update scripts/gen-proto.sh

--- scripts/gen-proto.sh
+++ scripts/gen-proto.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-CURRENT_DIR=$1
+CURRENT_DIR=$(pwd)
+echo $CURRENT_DIR
 for x in $(find ${CURRENT_DIR}/protos/* -type d); do
-  protoc -I=${x} -I=${CURRENT_DIR}/protos -I /usr/local/include --go_out=plugins=grpc:${CURRENT_DIR} ${x}/*.proto
+  protoc -I=${x} -I=${CURRENT_DIR}/protos -I /usr/local/include --go_out=${CURRENT_DIR} \
+   --go-grpc_out=${CURRENT_DIR} ${x}/*.proto
+done
+
+for x in $(find ${CURRENT_DIR}/genproto/* -type d); do
+  ls ${x}/*.pb.go | xargs -n1 -IX bash -c 'sed s/,omitempty// X > X.tmp && mv X{.tmp,}'
 done
