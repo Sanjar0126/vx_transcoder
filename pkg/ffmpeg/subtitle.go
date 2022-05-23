4ab186e255b43ef8108be340ab19275b976b9802
Madiyor <abdukhashimov@yandex.ru>
2022-05-23 19:15:29 +0500

    fix: rename variale names

--- pkg/ffmpeg/subtitle.go
+++ pkg/ffmpeg/subtitle.go
@@ -20,15 +20,15 @@ func NewSubtitleAPI(cfg *config.Config, log logger.Logger) SubtitleAPI {
 }
 
 func (s *SubtitleAPI) GetSubtitleLayers(input string) ([]Streams, error) {
-	audioLayers, err := getLayers(s.cfg.ScriptsFolder, input, s.codecType)
+	subtitleLayers, err := getLayers(s.cfg.ScriptsFolder, input, s.codecType)
 	if err != nil {
 		s.log.Error("failed to retrieve audio layers", logger.Error(err))
 		return nil, err
 	}
 
-	s.log.Info("audio layers are retreived", logger.Any("audios", audioLayers))
+	s.log.Info("audio layers are retreived", logger.Any("audios", subtitleLayers))
 
-	return audioLayers, nil
+	return subtitleLayers, nil
 }
 
 func (s *SubtitleAPI) ExtractSubtitle(input string) {
