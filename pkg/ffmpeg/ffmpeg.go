c1a43eb2957d6fa0eb088e8712dce375e8fd579c
Madiyor <abdukhashimov@yandex.ru>
2022-05-23 14:40:15 +0500

    fix: add audio,video and subtitle apis

--- pkg/ffmpeg/ffmpeg.go
+++ pkg/ffmpeg/ffmpeg.go
@@ -8,6 +8,10 @@ import (
 type ToolFfmpeg struct {
 	cfg *config.Config
 	log logger.Logger
+
+	video    *VideoAPI
+	audio    *AudioAPI
+	subtitle *SubtitleAPI
 }
 
 func NewToolFfmpeg(cfg *config.Config, log logger.Logger) ToolFfmpeg {
@@ -15,4 +19,28 @@ func NewToolFfmpeg(cfg *config.Config, log logger.Logger) ToolFfmpeg {
 		cfg: cfg,
 		log: log,
 	}
+}
+
+func (t *ToolFfmpeg) Video() *VideoAPI {
+	if t.video == nil {
+		t.log.Fatal("video api is not initialized...")
+	}
+
+	return t.video
+}
+
+func (t *ToolFfmpeg) Audio() *AudioAPI {
+	if t.audio == nil {
+		t.log.Fatal("audio api is not initialized...")
+	}
+
+	return t.audio
+}
+
+func (t *ToolFfmpeg) Subtitle() *SubtitleAPI {
+	if t.subtitle == nil {
+		t.log.Fatal("subtitle api is not initialized...")
+	}
+
+	return t.subtitle
 }
