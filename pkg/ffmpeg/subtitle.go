4019947e80c4b87f47bcac71c262f42c5e98ced9
Madiyor <abdukhashimov@yandex.ru>
2022-05-23 19:11:10 +0500

    fix: add retreiving subtitle layers

--- pkg/ffmpeg/subtitle.go
+++ pkg/ffmpeg/subtitle.go
@@ -6,21 +6,31 @@ import (
 )
 
 type SubtitleAPI struct {
-	cfg *config.Config
-	log logger.Logger
+	cfg       *config.Config
+	log       logger.Logger
+	codecType string
 }
 
 func NewSubtitleAPI(cfg *config.Config, log logger.Logger) SubtitleAPI {
 	return SubtitleAPI{
-		cfg: cfg,
-		log: log,
+		cfg:       cfg,
+		log:       log,
+		codecType: "subtitle",
 	}
 }
 
-func (s *SubtitleAPI) ExtractSubtitle() {
+func (s *SubtitleAPI) GetSubtitleLayers(input string) ([]Streams, error) {
+	audioLayers, err := getLayers(s.cfg.ScriptsFolder, input, s.codecType)
+	if err != nil {
+		s.log.Error("failed to retrieve audio layers", logger.Error(err))
+		return nil, err
+	}
 
+	s.log.Info("audio layers are retreived", logger.Any("audios", audioLayers))
+
+	return audioLayers, nil
 }
 
-func (s *SubtitleAPI) GetSubtitleLayers() {
+func (s *SubtitleAPI) ExtractSubtitle(input string) {
 
 }
