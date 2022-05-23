b5c30a01ff828e356443251fbbda9ef9967e33a2
Madiyor <abdukhashimov@yandex.ru>
2022-05-23 19:15:11 +0500

    fix: add video layer retrieving method

--- pkg/ffmpeg/video.go
+++ pkg/ffmpeg/video.go
@@ -6,14 +6,16 @@ import (
 )
 
 type VideoAPI struct {
-	cfg *config.Config
-	log logger.Logger
+	cfg       *config.Config
+	log       logger.Logger
+	codecType string
 }
 
 func NewVideoAPI(cfg *config.Config, log logger.Logger) VideoAPI {
 	return VideoAPI{
-		cfg: cfg,
-		log: log,
+		cfg:       cfg,
+		log:       log,
+		codecType: "video",
 	}
 }
 
@@ -21,8 +23,16 @@ func (v *VideoAPI) GetVideoWidthAndHeight() {
 
 }
 
-func (v *VideoAPI) GetVideoLayers() {
+func (v *VideoAPI) GetVideoLayers(input string) ([]Streams, error) {
+	videoLayers, err := getLayers(v.cfg.ScriptsFolder, input, v.codecType)
+	if err != nil {
+		v.log.Error("failed to retrieve audio layers", logger.Error(err))
+		return nil, err
+	}
 
+	v.log.Info("audio layers are retreived", logger.Any("audios", videoLayers))
+
+	return videoLayers, nil
 }
 
 func (v *VideoAPI) GetVideoDuration() {
