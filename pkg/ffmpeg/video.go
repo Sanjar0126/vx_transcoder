b7bcea5b44ed42abeed17ab85f77ed3e79c81720
Madiyor <abdukhashimov@yandex.ru>
2022-05-24 20:53:53 +0500

    fix: add resizing video

--- pkg/ffmpeg/video.go
+++ pkg/ffmpeg/video.go
@@ -1,6 +1,10 @@
 package ffmpeg
 
 import (
+	"errors"
+	"fmt"
+	"os/exec"
+
 	"gitlab.com/samandarobidovfrd/voxe_transcoding_service/config"
 	"gitlab.com/samandarobidovfrd/voxe_transcoding_service/pkg/logger"
 )
@@ -9,16 +13,16 @@ type VideoAPI struct {
 	cfg *config.Config
 	log logger.Logger
 
-	codecType         string
-	defaultAudioChunk string
+	codecType        string
+	defaultChunkSize string
 }
 
 func NewVideoAPI(cfg *config.Config, log logger.Logger) VideoAPI {
 	return VideoAPI{
-		cfg:               cfg,
-		log:               log,
-		codecType:         "video",
-		defaultAudioChunk: "5",
+		cfg:              cfg,
+		log:              log,
+		codecType:        "video",
+		defaultChunkSize: "5",
 	}
 }
 
@@ -34,7 +38,38 @@ func (v *VideoAPI) GetVideoLayers(input string) ([]Stream, error) {
 	return videoLayers, nil
 }
 
-func (v *VideoAPI) ConvertVideo(input Stream) error {
+type ResizeVideoArgs struct {
+	Input       string
+	Width       string
+	Bitrate     string
+	InputObject Stream
+}
+
+func (v *VideoAPI) ResizeVideo(args ResizeVideoArgs) error {
+	var (
+		outputPath = fmt.Sprintf(
+			"%s/%s/videos/%sp/video.m3u8", v.cfg.OutputDir, args.Input, args.Width,
+		)
+
+		resizeVideoScript   = fmt.Sprintf("%s%s", v.cfg.ScriptsFolder, "/ffmpeg/resize_video.sh")
+		resizingWidthHeight = fmt.Sprintf("%s:-2", args.Width)
+	)
+
+	out, err := exec.Command(
+		"/bin/sh",
+		resizeVideoScript,
+		resizingWidthHeight,
+		args.Bitrate,
+		v.defaultChunkSize,
+		outputPath,
+	).Output()
+
+	if err != nil {
+		v.log.Error("failed to extract audio", logger.Error(err))
+		return errors.New("failed to extract audio")
+	}
+
+	v.log.Info("extract output", logger.String("output", string(out)))
 
 	return nil
 }
