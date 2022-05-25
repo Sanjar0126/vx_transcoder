25e3eb45e02789f8180dbbe115928ebab6a7b349
Madiyor <abdukhashimov@yandex.ru>
2022-05-25 13:25:23 +0500

    fix: add audio implementations

--- pkg/transcode/audio/encoder.go
+++ pkg/transcode/audio/encoder.go
@@ -2,21 +2,24 @@ package audio
 
 import (
 	"gitlab.com/samandarobidovfrd/voxe_transcoding_service/config"
+	"gitlab.com/samandarobidovfrd/voxe_transcoding_service/pkg/ffmpeg"
 	"gitlab.com/samandarobidovfrd/voxe_transcoding_service/pkg/logger"
 )
 
 type AudioLayerReader interface {
-	ExtractInfos()
+	ExtractLayers(input string) ([]ffmpeg.Stream, error)
 }
 
 type AuidoExtracter interface {
-	ExtractAudios()
+	ExtractAudio(input, lang string, inputObject ffmpeg.Stream) error
 	AudioLayerReader
 }
 
 type AudioObject struct {
 	cfg *config.Config
 	log logger.Logger
+
+	audio ffmpeg.AudioAPI
 }
 
 func NewAudioExtracter(cfg *config.Config, log logger.Logger) AuidoExtracter {
@@ -26,10 +29,22 @@ func NewAudioExtracter(cfg *config.Config, log logger.Logger) AuidoExtracter {
 	}
 }
 
-func (a *AudioObject) ExtractInfos() {
-	panic("not implemented yet")
+func (a *AudioObject) ExtractLayers(input string) ([]ffmpeg.Stream, error) {
+	streams, err := a.audio.GetAudioLayers(input)
+	if err != nil {
+		a.log.Error("failed to extract audio layers", logger.Error(err))
+		return streams, err
+	}
+
+	return streams, nil
 }
 
-func (a *AudioObject) ExtractAudios() {
-	panic("not implemented yet")
+func (a *AudioObject) ExtractAudio(input, lang string, inputObject ffmpeg.Stream) error {
+	err := a.audio.ExtractAudio(input, lang, inputObject)
+	if err != nil {
+		a.log.Error("failed to exract audios", logger.Error(err))
+		return err
+	}
+
+	return nil
 }
