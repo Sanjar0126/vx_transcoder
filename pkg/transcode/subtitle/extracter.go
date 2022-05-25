a63b519aaa56be511121b1e6ac9f9ecf5889644d
Madiyor <abdukhashimov@yandex.ru>
2022-05-25 13:30:15 +0500

    fix: add subtitle implementatoins

--- pkg/transcode/subtitle/extracter.go
+++ pkg/transcode/subtitle/extracter.go
@@ -2,21 +2,24 @@ package subtitle
 
 import (
 	"gitlab.com/samandarobidovfrd/voxe_transcoding_service/config"
+	"gitlab.com/samandarobidovfrd/voxe_transcoding_service/pkg/ffmpeg"
 	"gitlab.com/samandarobidovfrd/voxe_transcoding_service/pkg/logger"
 )
 
 type SubtitleLayerReader interface {
-	ExtractInfos()
+	GetSubtitleLayers(input string) ([]ffmpeg.Stream, error)
 }
 
 type SubtitleExtracter interface {
 	SubtitleLayerReader
-	ExtractSubtitles()
+	ExtractSubtitle(input, lang string, inputObject ffmpeg.Stream) error
 }
 
 type SubtitleObject struct {
 	cfg *config.Config
 	log logger.Logger
+
+	subtitle ffmpeg.SubtitleAPI
 }
 
 func NewSubtitleExtracter(cfg *config.Config, log logger.Logger) SubtitleExtracter {
@@ -26,10 +29,22 @@ func NewSubtitleExtracter(cfg *config.Config, log logger.Logger) SubtitleExtract
 	}
 }
 
-func (s *SubtitleObject) ExtractInfos() {
+func (s *SubtitleObject) GetSubtitleLayers(input string) ([]ffmpeg.Stream, error) {
+	streams, err := s.subtitle.GetSubtitleLayers(input)
+	if err != nil {
+		s.log.Error("failed to get subtitle layers", logger.Error(err))
+		return streams, err
+	}
 
+	return streams, nil
 }
 
-func (s *SubtitleObject) ExtractSubtitles() {
+func (s *SubtitleObject) ExtractSubtitle(input, lang string, inputObject ffmpeg.Stream) error {
+	err := s.subtitle.ExtractSubtitle(input, lang, inputObject)
+	if err != nil {
+		s.log.Error("failed to extract subtitles", logger.Error(err))
+		return err
+	}
 
+	return nil
 }
